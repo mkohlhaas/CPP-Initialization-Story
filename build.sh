174a317bd8cd7576fd39eb0ab540106b553da14c
Hans-J. Schmid <knock@myopendoor.de>
2025-03-18 22:28:57 +0100

    meson build

--- build.sh
+++ build.sh
@@ -1,87 +1,299 @@
-mv "./ch01/ex 1 2 setting to zero.cpp"                                                    ./ch01/ex_1_2_setting_to_zero.cpp                               
-mv "./ch01/ex 1 1 simple car info.cpp"                                                    ./ch01/ex_1_1_simple_car_info.cpp                               
-mv "./ch01/ex 1 3 aggregates.cpp"                                                         ./ch01/ex_1_3_aggregates.cpp                                    
-mv "./ch01/ex 1 4 aggregates car info.cpp"                                                ./ch01/ex_1_4_aggregates_car_info.cpp                           
-mv "./ch01/ex 1 5 default member init.cpp"                                                ./ch01/ex_1_5_default_member_init.cpp                           
-mv "./ch13/ex 13 2 optional introduction.cpp"                                             ./ch13/ex_13_2_optional_introduction.cpp                        
-mv "./ch13/ex 13 3 instance counter.cpp"                                                  ./ch13/ex_13_3_instance_counter.cpp                             
-mv "./ch13/ex 13 6 singleton.cpp"                                                         ./ch13/ex_13_6_singleton.cpp                                    
-mv "./ch13/ex 13 5 multiple initializations.cpp"                                          ./ch13/ex_13_5_multiple_initializations.cpp                     
-mv "./ch13/ex 13 4 crtp instance counter.cpp"                                             ./ch13/ex_13_4_crtp_instance_counter.cpp                        
-mv "./ch13/ex 13 1 strong types.cpp"                                                      ./ch13/ex_13_1_strong_types.cpp                                 
-mv "./ch05/ex 5 7 fixing memory leak.cpp"                                                 ./ch05/ex_5_7_fixing_memory_leak.cpp                            
-mv "./ch05/ex 5 2 nested destructor calls.cpp"                                            ./ch05/ex_5_2_nested_destructor_calls.cpp                       
-mv "./ch05/ex 5 1 logging destructor.cpp"                                                 ./ch05/ex_5_1_logging_destructor.cpp                            
-mv "./ch05/ex 5 6 a memory leak.cpp"                                                      ./ch05/ex_5_6_a_memory_leak.cpp                                 
-mv "./ch05/ex 5 4 virtual destructor.cpp"                                                 ./ch05/ex_5_4_virtual_destructor.cpp                            
-mv "./ch05/ex 5 3 a pointer member.cpp"                                                   ./ch05/ex_5_3_a_pointer_member.cpp                              
-mv "./ch05/ex 5 8 compiler generated destructor.cpp"                                      ./ch05/ex_5_8_compiler_generated_destructor.cpp                 
-mv "./ch05/ex 5 5 partial object creation.cpp"                                            ./ch05/ex_5_5_partial_object_creation.cpp                       
-mv "./ch06/ex 6 2 printing type info.cpp"                                                 ./ch06/ex_6_2_printing_type_info.cpp                            
-mv "./ch06/ex 6 1 trying a correct type for map elements.cpp"                             ./ch06/ex_6_1_trying_a_correct_type_for_map_elements.cpp        
-mv "./ch06/ex 6 3 iterating through map structured binding.cpp"                           ./ch06/ex_6_3_iterating_through_map_structured_binding.cpp      
-mv "./ch06/ex 6 4 loops and ub.cpp"                                                       ./ch06/ex_6_4_loops_and_ub.cpp                                  
-mv "./ch03/ex 3 6 copy on vec resize.cpp"                                                 ./ch03/ex_3_6_copy_on_vec_resize.cpp                            
-mv "./ch03/ex 3 9 move assignment for product.cpp"                                        ./ch03/ex_3_9_move_assignment_for_product.cpp                   
-mv "./ch03/ex 3 4 non default copy constructor.cpp"                                       ./ch03/ex_3_4_non_default_copy_constructor.cpp                  
-mv "./ch03/ex 3 3 copy elision cpp17.cpp"                                                 ./ch03/ex_3_3_copy_elision_cpp17.cpp                                  
-mv "./ch03/ex 3 1 logging in copy constructor.cpp"                                        ./ch03/ex_3_1_logging_in_copy_constructor.cpp                   
-mv "./ch03/ex 3 8 copy assignment for product.cpp"                                        ./ch03/ex_3_8_copy_assignment_for_product.cpp                   
-mv "./ch03/ex 3 7 move on vec resize.cpp"                                                 ./ch03/ex_3_7_move_on_vec_resize.cpp                            
-mv "./ch03/ex 3 5 move constructor.cpp"                                                   ./ch03/ex_3_5_move_constructor.cpp                              
-mv "./ch03/ex 3 10 logging in data packet.cpp"                                            ./ch03/ex_3_10_logging_in_data_packet.cpp                       
-mv "./ch03/ex 3 2 named copy elision.cpp"                                                 ./ch03/ex_3_2_named_copy_elision.cpp                            
-mv "./ch12/ex 12 7 json like.cpp"                                                         ./ch12/ex_12_7_json_like.cpp                                    
-mv "./ch12/ex 12 5 emplace.cpp"                                                           ./ch12/ex_12_5_emplace.cpp                                      
-mv "./ch12/ex 12 4 make_unique.cpp"                                                       ./ch12/ex_12_4_make_unique.cpp                                  
-mv "./ch12/ex 12 2 brace elision.cpp"                                                     ./ch12/ex_12_2_brace_elision.cpp                                
-mv "./ch12/ex 12 1 aggregate classes several examples.cpp"                                ./ch12/ex_12_1_aggregate_classes_several_examples.cpp           
-mv "./ch12/ex 12 6 designated init demo.cpp"                                              ./ch12/ex_12_6_designated_init_demo.cpp                         
-mv "./ch12/ex 12 3 initialization with round parens.cpp"                                  ./ch12/ex_12_3_initialization_with_round_parens.cpp             
-mv "./ch04/ex 4 5 inheriting constructors and protected section.cpp"                      ./ch04/ex_4_5_inheriting_constructors_and_protected_section.cpp 
-mv "./ch04/ex 4 1 delegating constructors.cpp"                                            ./ch04/ex_4_1_delegating_constructors.cpp                       
-mv "./ch04/ex 4 4 inheriting constructors.cpp"                                            ./ch04/ex_4_4_inheriting_constructors.cpp                       
-mv "./ch04/ex 4 2 inheritance.cpp"                                                        ./ch04/ex_4_2_inheritance.cpp                                   
-mv "./ch04/ex 4 3 base class constructor order.cpp"                                       ./ch04/ex_4_3_base_class_constructor_order.cpp                  
-mv "./ch11/ex 11 4 thread_local.cpp"                                                      ./ch11/ex_11_4_thread_local.cpp                                 
-mv "./ch11/ex 11 8 constinit pair.cpp"                                                    ./ch11/ex_11_8_constinit_pair.cpp                               
-mv "./ch11/ex 11 1 static and automatic.cpp"                                              ./ch11/ex_11_1_static_and_automatic.cpp                         
-mv "./ch11/ex 11 5 beging and end of a thread_local.cpp"                                  ./ch11/ex_11_5_beging_and_end_of_a_thread_local.cpp             
-mv "./ch11/ex 11 6 static initialization.cpp"                                             ./ch11/ex_11_6_static_initialization.cpp                        
-mv "./ch02/ex 2 1 simple data packet class.cpp"                                           ./ch02/ex_2_1_simple_data_packet_class.cpp                      
-mv "./ch02/ex 2 7 adding constructors to packet.cpp"                                      ./ch02/ex_2_7_adding_constructors_to_packet.cpp                 
-mv "./ch02/ex 2 5 multiple arguments and braces.cpp"                                      ./ch02/ex_2_5_multiple_arguments_and_braces.cpp                 
-mv "./ch02/ex 2 4 most vexing parse error.cpp"                                            ./ch02/ex_2_4_most_vexing_parse_error.cpp                       
-mv "./ch02/ex 2 9 default and delete constructors.cpp"                                    ./ch02/ex_2_9_default_and_delete_constructors.cpp               
-mv "./ch02/ex 2 10 implicit conversions.cpp"                                              ./ch02/ex_2_10_implicit_conversions.cpp                         
-mv "./ch02/ex 2 3 constructor for product.cpp"                                            ./ch02/ex_2_3_constructor_for_product.cpp                       
-mv "./ch02/ex 2 2 value init for data packet.cpp"                                         ./ch02/ex_2_2_value_init_for_data_packet.cpp                    
-mv "./ch02/ex 2 6 logging in constructors.cpp"                                            ./ch02/ex_2_6_logging_in_constructors.cpp                       
-mv "./ch02/ex 2 8 implicit default constructor.cpp"                                       ./ch02/ex_2_8_implicit_default_constructor.cpp                  
-mv "./ch09/ex 9 3 function taking initializer_list.cpp"                                   ./ch09/ex_9_3_function_taking_initializer_list.cpp              
-mv "./ch09/ex 9 5 package class demo.cpp"                                                 ./ch09/ex_9_5_package_class_demo.cpp                            
-mv "./ch09/ex 9 4 constructor taking initializer_list.cpp"                                ./ch09/ex_9_4_constructor_taking_initializer_list.cpp           
-mv "./ch09/ex 9 7 extra copy with initializer_list.cpp"                                   ./ch09/ex_9_7_extra_copy_with_initializer_list.cpp              
-mv "./ch09/ex 9 1 basic syntax.cpp"                                                       ./ch09/ex_9_1_basic_syntax.cpp                                  
-mv "./ch09/ex 9 8 trying to pass unique_ptr.cpp"                                          ./ch09/ex_9_8_trying_to_pass_unique_ptr.cpp                     
-mv "./ch09/ex 9 10 initialization from a list of unique_ptr.cpp"                          ./ch09/ex_9_10_initialization_from_a_list_of_unique_ptr.cpp     
-mv "./ch09/ex 9 9 variadic function template.cpp"                                         ./ch09/ex_9_9_variadic_function_template.cpp                    
-mv "./ch09/ex 9 6 package class with initializer_list.cpp"                                ./ch09/ex_9_6_package_class_with_initializer_list.cpp           
-mv "./ch09/ex 9 2 containers and nsdmi.cpp"                                               ./ch09/ex_9_2_containers_and_nsdmi.cpp                          
-mv "./ch10/ex 10 3 unique_ptr as data member.cpp"                                         ./ch10/ex_10_3_unique_ptr_as_data_member.cpp                    
-mv "./ch10/ex 10 6 reference as data member.cpp"                                          ./ch10/ex_10_6_reference_as_data_member.cpp                     
-mv "./ch10/ex 10 7 reference_wrapper as data member.cpp"                                  ./ch10/ex_10_7_reference_wrapper_as_data_member.cpp             
-mv "./ch10/ex 10 1 constant data member.cpp"                                              ./ch10/ex_10_1_constant_data_member.cpp                         
-mv "./ch10/ex 10 5 shared_ptr as data member.cpp"                                         ./ch10/ex_10_5_shared_ptr_as_data_member.cpp                    
-mv "./ch10/ex 10 8 showing basic props.cpp"                                               ./ch10/ex_10_8_showing_basic_props.cpp                          
-mv "./ch10/ex 10 2 raw pointer as data member.cpp"                                        ./ch10/ex_10_2_raw_pointer_as_data_member.cpp                   
-mv "./ch08/ex 8 3 calling init functions.cpp"                                             ./ch08/ex_8_3_calling_init_functions.cpp                        
-mv "./ch08/ex 8 5 dependency in initializers.cpp"                                         ./ch08/ex_8_5_dependency_in_initializers.cpp                    
-mv "./ch08/ex 8 7 move constructors and nsdmi.cpp"                                        ./ch08/ex_8_7_move_constructors_and_nsdmi.cpp                   
-mv "./ch08/ex 8 10 direct initialization and parens.cpp"                                  ./ch08/ex_8_10_direct_initialization_and_parens.cpp             
-mv "./ch08/ex 8 2 basics of initialization.cpp"                                           ./ch08/ex_8_2_basics_of_initialization.cpp                      
-mv "./ch08/ex 8 4 various nsdmi syntax.cpp"                                               ./ch08/ex_8_4_various_nsdmi_syntax.cpp                          
-mv "./ch08/ex 8 9 bit fields and nsdmi.cpp"                                               ./ch08/ex_8_9_bit_fields_and_nsdmi.cpp                          
-mv "./ch08/ex 8 8 aggregates and nsdmi.cpp"                                               ./ch08/ex_8_8_aggregates_and_nsdmi.cpp                          
-mv "./ch08/ex 8 1 nsdmi basics.cpp"                                                       ./ch08/ex_8_1_nsdmi_basics.cpp                                  
-mv "./ch08/ex 8 6 copy constructors and nsdmi.cpp"                                        ./ch08/ex_8_6_copy_constructors_and_nsdmi.cpp                   
+cd ch01
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd ..
+cd ch02
+cd 01
+meson setup build
+cd ..
+cd 02
+meson setup build
+cd ..
+cd 03
+meson setup build
+cd ..
+cd 04
+meson setup build
+cd ..
+cd 05
+meson setup build
+cd ..
+cd 06
+meson setup build
+cd ..
+cd 07
+meson setup build
+cd ..
+cd 08
+meson setup build
+cd ..
+cd 09
+meson setup build
+cd ..
+cd 10
+meson setup build
+cd ..
+cd ..
+cd ch03
+cd 01
+meson setup build
+cd ..
+cd 02
+meson setup build
+cd ..
+cd 03
+meson setup build
+cd ..
+cd 04
+meson setup build
+cd ..
+cd 05
+meson setup build
+cd ..
+cd 06
+meson setup build
+cd ..
+cd 07
+meson setup build
+cd ..
+cd 08
+meson setup build
+cd ..
+cd 09
+meson setup build
+cd ..
+cd 10
+meson setup build
+cd ..
+cd ..
+cd ch04
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd ..
+cd ch05
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd 6
+meson setup build
+cd ..
+cd 7
+meson setup build
+cd ..
+cd 8
+meson setup build
+cd ..
+cd ..
+cd ch06
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd ..
+cd ch08
+cd 01
+meson setup build
+cd ..
+cd 02
+meson setup build
+cd ..
+cd 03
+meson setup build
+cd ..
+cd 04
+meson setup build
+cd ..
+cd 05
+meson setup build
+cd ..
+cd 06
+meson setup build
+cd ..
+cd 07
+meson setup build
+cd ..
+cd 08
+meson setup build
+cd ..
+cd 09
+meson setup build
+cd ..
+cd 10
+meson setup build
+cd ..
+cd ..
+cd ch09
+cd 01
+meson setup build
+cd ..
+cd 02
+meson setup build
+cd ..
+cd 03
+meson setup build
+cd ..
+cd 04
+meson setup build
+cd ..
+cd 05
+meson setup build
+cd ..
+cd 06
+meson setup build
+cd ..
+cd 07
+meson setup build
+cd ..
+cd 08
+meson setup build
+cd ..
+cd 09
+meson setup build
+cd ..
+cd 10
+meson setup build
+cd ..
+cd ..
+cd ch10
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd 6
+meson setup build
+cd ..
+cd 7
+meson setup build
+cd ..
+cd 8
+meson setup build
+cd ..
+cd ..
+cd ch11
+cd 1
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd 6
+meson setup build
+cd ..
+cd 8
+meson setup build
+cd ..
+cd ..
+cd ch12
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd 6
+meson setup build
+cd ..
+cd 7
+meson setup build
+cd ..
+cd ..
+cd ch13
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd 5
+meson setup build
+cd ..
+cd 6
+meson setup build
+cd ..
+cd ..
+cd ch14
+cd 1
+meson setup build
+cd ..
+cd 2
+meson setup build
+cd ..
+cd 3
+meson setup build
+cd ..
+cd 4
+meson setup build
+cd ..
+cd ..
